94aec964e659311e9f66fc932257b15fbaee1ec3
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-06-27 20:05:09 +0530

    constants and shadowing

--- DataTypes_Variables/src/main.rs
+++ DataTypes_Variables/src/main.rs
@@ -13,15 +13,32 @@ fn main() {
     
     let mut z: i8 = 20;
     
-    println!("{}",x);
-    println!("{}",y);
-    println!("{}",z);
+    println!("Value of x inital decleration is: {}",x);
+    println!("Value of y inital decleration is: {}",y);
+    println!("Value of z inital decleration is: {}",z);
 
     z= 25; // in this the value of z is update to 25 because we have make that mutable while decalaration of the varible.
     
-    println!("{}",z);
+    println!("Value of z are changing the value throug mut is: {}",z);
 
     let a: f32 = -1323.0331;  //this is for storing the floating number . It can store both positve and negative number also
 
-    println!("{}",a);
+    println!("Value of a floating number is: {}",a);
+
+    const SECONDS_IN_MIN: u32 =60;
+    //IN rust the naming of contants are always in capital letters
+    
+    //Constants are not only inmutable by default. But they are always inmutable.
+
+    println!("Value of the constant is: {}",SECONDS_IN_MIN);
+
+    //Shadowing
+
+    //Previously x was declare as 5 so we are going to use that same in shadowing also
+    let x = x + 2;  //increment of x by 2
+    {
+        let x = x * 2; //value of the x is updated and stored in x will be accessed by this bolck only the value can not be accessed form the outside.
+        println!("Value of x in the inner block is: {}",x);
+    }
+    println!("Value of x outside the block is: {}",x);
 }
