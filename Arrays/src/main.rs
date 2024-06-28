5988d7355889a803927d386297f58d8bae431f2c
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-06-28 19:56:05 +0530

    feat: Add Arrays module and initial implementation

---
+++ Arrays/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // Collection of multiple values of same data-types 
+    // Can store only values of same type , fixed length
+
+    let a = [1,2,3,4];
+    
+    //the same can be declared in the different way for example: 
+    let b: [i32;5]=[1,2,3,4,5];   // i32 is type of element and after the semicolon number of elements.
+    
+    // We can also initialise the arrays of same emelent using let a:[2;5] this array will contain 2 five times [2,2,2,2,2].
+    
+
+}
